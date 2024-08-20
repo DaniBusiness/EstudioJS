c4859e69c498f8f9497ad5e9e1dab9ce958b5df2
DaniBusiness <daniele.berta7@gmail.com>
2024-08-20 16:55:03 +0200

    ejercicios de numero mas Grande

--- ejemplos-paginas/ejercicios de variables/js/arrays.js
+++ ejemplos-paginas/ejercicios de variables/js/arrays.js
@@ -1,1 +1,41 @@
 // Hacer un código que tome los elementos de un array de números, y me devuelva el más chico y el más grande de esos números
+
+var numeros = [2,6,14,9]
+
+function myChico() {
+    // console.log("dados");
+    var numeroChico = numeros[0];
+    console.log(typeof(numeroChico));
+    for(let j=1; j<numeros.length; j++) {
+        console.log(numeros[j+1]);
+        if (numeros[j]<numeros[j+1]) {
+            if (numeros[j]<numeroChico) {
+                numeroChico=numeros[j];
+            }
+        } else {
+            if (numeros[j+1]<numeroChico) {
+            numeroChico=numeros[j+1];
+            }  
+        }         
+    }
+    return numeroChico;
+}
+
+function myGrande() {
+    // console.log("dados");
+    var numeroGrande = numeros[0];
+    console.log(typeof(numeroGrande));
+    for(let j=1; j<numeros.length; j++) {
+        console.log(numeros[j+1]);
+        if (numeros[j]>numeros[j+1]) {
+            if (numeros[j]>numeroGrande) {
+                numeroChico=numeros[j];
+            }
+        } else {
+            if (numeros[j+1]>numeroGrande) {
+            numeroGrande=numeros[j+1];
+            }  
+        }         
+    }
+    return numeroGrande;
+}
